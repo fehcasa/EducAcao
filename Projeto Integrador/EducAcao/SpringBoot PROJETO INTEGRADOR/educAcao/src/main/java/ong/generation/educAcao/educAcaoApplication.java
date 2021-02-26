44e49b1f0806a2b58ed699205026fe64e357c145
Felipe Casagrande <77506504+fehcasa@users.noreply.github.com>
2021-02-26 14:33:12 -0300

    Add files via upload

---
+++ Projeto Integrador/EducAcao/SpringBoot PROJETO INTEGRADOR/educAcao/src/main/java/ong/generation/educAcao/educAcaoApplication.java
@@ -0,0 +1,13 @@
+package ong.generation.educAcao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class educAcaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(educAcaoApplication.class, args);
+	}
+
+}
